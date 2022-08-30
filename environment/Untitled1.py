51349dd6138be2da5766fb410d68788485d12982
mariacastellano <105322520+mariacastellano@users.noreply.github.com>
2022-08-30 19:30:28 -0300

    Add files via upload

---
+++ environment/Untitled1.py
@@ -0,0 +1,8 @@
+print("Numero de horas de trabajo hoy? Coste por hora?")
+input("hora y coste")
+
+horas =input("Cant de horas:")
+coste =input("Coste por hora:")
+pago = hora * coste
+
+print("Su pago es", pago)
